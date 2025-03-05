5645255d01ef2bf3278cd897b45e69e564883e86
maomaocris <53568888+maomaocris@users.noreply.github.com>
2025-03-06 00:16:58 +0800

    Update serv00.sh

--- serv00.sh
+++ serv00.sh
@@ -1,4 +1,4 @@
-#wget -O- https://github.com/go-gost/gost/releases/download/v3.0.0/gost_3.0.0_freebsd_amd64.tar.gz | tar xzv gost
+wget -O- https://github.com/go-gost/gost/releases/download/v3.0.0/gost_3.0.0_freebsd_amd64.tar.gz | tar xzv gost
 if pgrep -x gost > /dev/null
 then
     echo "gost 已经运行"
@@ -7,7 +7,7 @@ else
     # 启动程序
     nohup ./gost -L socks5://:19823 >/dev/null 2>&1 &
 fi
-#wget -O cloudflared https://github.com/QWsudo/serv00-keepalive/releases/download/singbox/cloudflared && chmod +x cloudflared
+wget -O cloudflared https://github.com/QWsudo/serv00-keepalive/releases/download/singbox/cloudflared && chmod +x cloudflared
 if pgrep -x cloudflared > /dev/null
 then
     echo "cloudflared 已经运行"
@@ -16,5 +16,6 @@ else
     # 启动程序
     nohup ./cloudflared tunnel --logfile ./cf.log --protocol http2 run --token eyJhIjoiM2QyNjE2NTkzM2Y0Zjk5MTc5NDRhNjIwOGUxZmRhYmIiLCJ0IjoiN2JkNjY0NWQtYjdjOC00ZDcyLWE0ZDQtMGZkYTM3YWVjYWQ3IiwicyI6Ill6UmtZVE5tWXpFdE1ERTJaaTAwTVdNNExXRTJZVEl0TTJReU5HWXlaRFptWVRNNCJ9 >/dev/null 2>&1 &
 fi
-#devil www add hello.doinbgodb.serv00.net nodejs /usr/local/bin/node22
+#devil www del crismao.serv00.net
+#devil www add crismao.serv00.net nodejs /usr/local/bin/node22
 exit
