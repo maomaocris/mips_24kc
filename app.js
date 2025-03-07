f3b718ce79f3c0bd320519863febedc84505426a
maomaocris <53568888+maomaocris@users.noreply.github.com>
2025-03-07 14:38:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -37,12 +37,12 @@ app.get("/status", function (req, res) {
 });
 
 // 只允许访问 /info 页面，其他页面返回 404
-app.use((req, res, next) => {
-    if (req.path === '/info') {
-        return next(); // 如果是 /info 页面，继续处理后续路由
-    }
-    res.status(404).send('页面未找到');
-});
+//app.use((req, res, next) => {
+    //if (req.path === '/info') {
+        //return next(); // 如果是 /info 页面，继续处理后续路由
+    //}
+    //res.status(404).send('页面未找到');
+//});
 
 app.listen(3000, () => {
     console.log("服务器已启动，监听端口 3000");
